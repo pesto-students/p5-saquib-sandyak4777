9eaca3bd35ad4df7421e8087d8c5f6bb4874f920
sandyak4777 <sandeshak4777@gmail.com>
2022-09-24 13:20:15 +0530

    initial code setup for spiral array

---
+++ Week-6/exercise6.2/spiral.js
@@ -0,0 +1,19 @@
+let array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+
+const spiralArray = (array) =>{
+    let left=0;
+    let top=0;
+    const result = [];
+    let right =array[0].length-1;
+    let bottom= array.length-1;
+    let direction='right';
+    return array
+}
+
+
+console.log(spiralArray(array));
