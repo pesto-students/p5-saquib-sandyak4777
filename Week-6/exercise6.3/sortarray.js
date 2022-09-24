670d74be04f1b41aaf809468841da77b9da32a12
sandyak4777 <sandeshak4777@gmail.com>
2022-09-24 15:41:22 +0530

    custom sorting

--- Week-6/exercise6.3/sortarray.js
+++ Week-6/exercise6.3/sortarray.js
@@ -1,1 +1,23 @@
 const array = [0, 1, 2, 0, 1, 2, 0, 0, 1, 2, 1, 0, 1, 2, 2];
+
+// console.log(array.sort()) /// done by inbuilt function
+
+///custom sorting by function
+const sortByFunction = (length, array) => {
+  for (i = 0; i < length; i++) {
+    for (j = 0; j < length; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }
+    }
+  }
+
+  return array;
+};
+
+const len = array.length;
+const Result = sortByFunction(len, array);
+
+console.log("array is sorted", Result);
