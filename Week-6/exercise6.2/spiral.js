5ec30c8399f1ac062e6ab3bf132483f777726139
sandyak4777 <sandeshak4777@gmail.com>
2022-09-24 13:36:28 +0530

    downward direction

--- Week-6/exercise6.2/spiral.js
+++ Week-6/exercise6.2/spiral.js
@@ -9,12 +9,22 @@ const spiralArray = (array) =>{
     let left=0;
     let top=0;
     const result = [];
-    let right =array[0].length-1;//3
-    let bottom= array.length-1;//3
+    let right =array[0].length-1;//3-1=2//column
+    let bottom= array.length-1;//3-1=2//row
     let direction='right';
 
     while(left<=right && top<=bottom){
-        
+        if(direction==="right"){
+            for(i=left; i<=right; i++){
+                result.push(array[top][i]);
+            }
+            direction="down";
+            top=+1;
+        }else if(direction==="down"){
+            for(i=top;i<=bottom;i++){
+                result.push(array[i][right])
+            }
+        }
     }
     return result
 }
