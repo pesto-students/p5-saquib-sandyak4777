3289e5357bec90a60ed423052208f56fc166f112
sandyak4777 <sandeshak4777@gmail.com>
2022-09-25 09:55:06 +0530

    defining heapify function

--- Week-6/exercise6.3/heapsort.js
+++ Week-6/exercise6.3/heapsort.js
@@ -1,9 +1,27 @@
 
+const heapify = (arr, length, i) => {
+    let largest = i
+    const left = i * 2 + 1
+    const right = left + 1
+  
+  
+    return arr
+  }
 
  const heapSort = arr => {
     const length = arr.length
     let i = Math.floor(length / 2 - 1)
     let k = length - 1
+    while (i >= 0) {
+        heapify(arr, length, i)
+        i--
+      }
+    
+      while (k >= 0) {
+        [arr[0], arr[k]] = [arr[k], arr[0]]
+        heapify(arr, k, 0)
+        k--
+      }
   
     return arr
   }
