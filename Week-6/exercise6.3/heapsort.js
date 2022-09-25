3a9991c1854aef3ebcdc25e8c8408e6b0e018aff
sandyak4777 <sandeshak4777@gmail.com>
2022-09-25 09:53:52 +0530

    defining heapsort function

--- Week-6/exercise6.3/heapsort.js
+++ Week-6/exercise6.3/heapsort.js
@@ -1,3 +1,13 @@
+
+
+ const heapSort = arr => {
+    const length = arr.length
+    let i = Math.floor(length / 2 - 1)
+    let k = length - 1
+  
+    return arr
+  }
+
 const arr = [0,0,1,2,2,2,2,1,1,1,1,0,0,0,0,0,1,2,1,3,4,2,1,3,4,0,1,2];
 sortedArr = heapSort(arr);
 
