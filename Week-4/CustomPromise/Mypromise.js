daae00b0ad42b966ada4e0297bd1d788ee4ed129
sandyak4777 <sandeshak4777@gmail.com>
2022-09-28 22:52:07 +0530

    removed commented lines

--- Week-4/CustomPromise/Mypromise.js
+++ Week-4/CustomPromise/Mypromise.js
@@ -1,126 +1,121 @@
 const STATE = {
-    PENDING: 'PENDING',
-    FULFILLED: 'FULFILLED',
-    REJECTED: 'REJECTED',
+  PENDING: "PENDING",
+  FULFILLED: "FULFILLED",
+  REJECTED: "REJECTED",
+};
+
+class MyPromise {
+  #thenCallBacks = [];
+  #catchCallBacks = [];
+  #state = STATE.PENDING;
+  #value;
+  #onSuccessBind = this.#onSuccess.bind(this);
+  #onFailBind = this.#onFail.bind(this);
+  constructor(callbackCode) {
+    try {
+      callbackCode(this.#onSuccessBind, this.#onFailBind);
+    } catch (err) {
+      this.#onFail(e);
+    }
   }
 
-class MyPromise{
-    #thenCallBacks= [];
-    #catchCallBacks=[];
-    #state= STATE.PENDING;
-    #value 
-    #onSuccessBind = this.#onSuccess.bind(this)
-    #onFailBind = this.#onFail.bind(this)
-    constructor(callbackCode){
-        try{
-            callbackCode(this.#onSuccessBind, this.#onFailBind)
-        }catch(err){
-            this.#onFail(e)
+  #runCallBacks() {
+    if (this.#state === STATE.FULFILLED) {
+      this.#thenCallBacks.forEach((callback) => {
+        callback(this.#value);
+      });
+      this.#thenCallBacks = [];
+    }
+    if (this.#state === STATE.REJECTED) {
+      this.#catchCallBacks.forEach((callback) => {
+        callback(this.#value);
+      });
+      this.#catchCallBacks = [];
+    }
+  }
+
+  #onSuccess(value) {
+    queueMicrotask(() => {
+      if (this.#state !== STATE.PENDING) return;
+
+      if (value instanceof MyPromise) {
+        value.then(this.#onSuccessBind, this.#onFailBind);
+        return;
+      }
+      this.#value = value;
+      this.#state = STATE.FULFILLED;
+      this.#runCallBacks();
+    });
+  }
+  #onFail(value) {
+    queueMicrotask(() => {
+      if (this.#state !== STATE.PENDING) return;
+
+      if (value instanceof MyPromise) {
+        value.then(this.#onSuccessBind, this.#onFailBind);
+        return;
+      }
+
+      this.#value = value;
+      this.#state = STATE.REJECTED;
+      this.#runCallBacks();
+    });
+  }
+
+  then(thenCallBack, catchCallBack) {
+    return new MyPromise((resolve, reject) => {
+      this.#thenCallBacks.push((result) => {
+        if (thenCallBack == null) {
+          resolve(result);
+          return;
         }
-    }
-
-    #runCallBacks(){
-        if(this.#state === STATE.FULFILLED){
-            this.#thenCallBacks.forEach((callback)=>{
-                callback(this.#value)
-            })
-            this.#thenCallBacks=[]
+        try {
+          resolve(thenCallBack(result));
+        } catch (err) {
+          reject(err);
         }
-        if(this.#state === STATE.REJECTED){
-            this.#catchCallBacks.forEach((callback)=>{
-                callback(this.#value)
-            })
-            this.#catchCallBacks=[]
+      });
+      this.#catchCallBacks.push((result) => {
+        if (catchCallBack == null) {
+          reject(result);
+          return;
         }
-    }
-
-    #onSuccess(value){
-        queueMicrotask(()=>{
-            if(this.#state !== STATE.PENDING) return
-
-        if(value instanceof MyPromise){
-            value.then(this.#onSuccessBind,this.#onFailBind)
-            return
+        try {
+          resolve(thenCallBack(result));
+        } catch (err) {
+          reject(err);
         }
-        this.#value=value;
-        this.#state=STATE.FULFILLED
-        this.#runCallBacks()
-        })
-    }
-    #onFail(value){
-        queueMicrotask(()=>{
-            if(this.#state !== STATE.PENDING) return
+      });
+      this.#runCallBacks();
+    });
+  }
+  catch(callbackCode) {
+    return this.then(undefined, callbackCode);
+  }
+  finally(callbackCode) {
+    return this.then(
+      (result) => {
+        callbackCode();
+        return result;
+      },
+      (result) => {
+        callbackCode();
+        throw result;
+      }
+    );
+  }
 
-        if(value instanceof MyPromise){
-            value.then(this.#onSuccessBind,this.#onFailBind)
-            return
-        }
+  static resolve(value) {
+    return new Promise((resolve) => {
+      resolve(value);
+    });
+  }
 
-        this.#value=value;
-        this.#state=STATE.REJECTED
-        this.#runCallBacks()
-        })
-    }
-
-    then(thenCallBack,catchCallBack){
-        return new MyPromise((resolve, reject)=>{
-            this.#thenCallBacks.push(result=>{
-                if(thenCallBack == null){
-                    resolve(result)
-                    return 
-                }
-                try{
-                    resolve(thenCallBack(result))
-                }catch(err){
-                    reject(err)
-                }
-            })
-            this.#catchCallBacks.push(result=>{
-                if(catchCallBack == null){
-                    reject(result)
-                    return 
-                }
-                try{
-                    resolve(thenCallBack(result))
-                }catch(err){
-                    reject(err)
-                }
-            }) 
-            this.#runCallBacks()
-        })
-        // if(thenCallBack !== null) this.#thenCallBacks.push(this.#thenCallBack) ;
-        // if(catchCallBack !== null) this.#catchCallBacks.push(this.#catchCallBack) 
-        // this.#thenCallBacks.push(callbackCode)
-
-        
-        // this.#runCallBacks()
-    }
-    catch(callbackCode){
-      return  this.then(undefined,callbackCode)
-    }
-    finally(callbackCode){
-        return this.then(result=>{
-            callbackCode()
-            return result
-        },result=>{
-            callbackCode()
-            throw result
-        }
-        )
-    }
-
-    static resolve(value){
-        return new Promise((resolve)=>{
-            resolve(value)
-        })
-    }
-
-    static reject(value){
-        return new Promise((resolve , reject)=>{
-            reject(value)
-        })
-    }
+  static reject(value) {
+    return new Promise((resolve, reject) => {
+      reject(value);
+    });
+  }
 }
 
-
-module.exports= MyPromise
+module.exports = MyPromise;
--- Week-4/CustomPromise/solvedByCustom.js
+++ Week-4/CustomPromise/solvedByCustom.js
@@ -1,23 +1,22 @@
-//Solving by custom  
+//Solving by custom
 // imported my custom promise
-const customPromise = require('./Mypromise')
+const customPromise = require("./Mypromise");
 
-const getNumber = () =>{
-    const randomNum = parseInt(Math.random()*100,10);
-    return randomNum
-}
+const getNumber = () => parseInt(Math.random() * 100, 10);
 
-const customPromiseUsed= new customPromise((resolve,reject)=>{
-    const num=getNumber();
-    if(num%5===0){
-                reject(`The number is failure ${num}`)
-    }else{
-        resolve(`the number is success ${num}`);
-    }
-})
+const customPromiseUsed = new customPromise((resolve, reject) => {
+  const num = getNumber();
+  if (num % 5 === 0) {
+    reject(`The number is failure ${num}`);
+  } else {
+    resolve(`the number is success ${num}`);
+  }
+});
 
-customPromiseUsed.then((result)=>{
-    console.log('success',result)
-}).catch((err)=>{
-    console.log(err)
-})
+customPromiseUsed
+  .then((result) => {
+    console.log("success", result);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
