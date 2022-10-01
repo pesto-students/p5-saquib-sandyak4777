1fabf63b9bacf3166497ad050e6e8ce6e4102b75
sandyak4777 <sandeshak4777@gmail.com>
2022-10-01 18:23:06 +0530

    solved profit of stock problem

--- Week-6/exercise6.4/profitofstock.js
+++ Week-6/exercise6.4/profitofstock.js
@@ -1,6 +1,19 @@
+const sort = require('../exercise6.3/heapsort');
 
+const value = (prices, arr) => {
+  if (prices.lengh < 1) {
+    return (arr[0] = 0);
+  }
+  for (i = 0; i < prices.length; i++) {
+    let j = i;
+    while (j <= prices.length - 2) {
+      arr.push(prices[i] - prices[j + 1]);
+      j++;
+    }
+  }
+};
 
-
-const stockPrices =[7,1,5,3,6,4];
+const stockPrices = [4,9,5,0,7,10,1];
 const temperoryArray = [];
-const stockDifference = value(stockPrices,temperoryArray);
+value(stockPrices, temperoryArray);
+console.log("maximum profit", Math. abs(sort(temperoryArray)[0]));
