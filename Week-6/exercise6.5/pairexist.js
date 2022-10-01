5443346154f38a4a532e0bb2300d336695408450
sandyak4777 <sandeshak4777@gmail.com>
2022-10-01 19:53:46 +0530

    solution one

--- Week-6/exercise6.5/pairexist.js
+++ Week-6/exercise6.5/pairexist.js
@@ -1,8 +1,25 @@
-const a = [10,12,-30,15,30,60,70];
+const a = [10, 12, -30, 15, 30, 60, 70];
 const b = 40;
 
-const checkIfPairExist = (arr,valueTobeChecked) =>{
-    
-}
+const checkIfPairExist = (arr, valueTobeChecked) => {
+  if (!arr.length) {
+    return 0;
+  }
+  for (i = 0; i < arr.length; i++) {
+    let j = i;
+    while (j <= arr.length - 1) {
+      if (a[i] - a[j] == valueTobeChecked) {
+        console.log("pair of values",a[i],a[j])
+        return 1;
+      }
+      j++;
+    }
+  }
+};
 
-checkIfPairExist(a,b);
+const result = checkIfPairExist(a, b);
+
+console.log("result", result);
+
+//result 1
+//pair of values 10 , -30
