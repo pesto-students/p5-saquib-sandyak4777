41998d41071fa0276e9ab692501075b476ef019a
sandyak4777 <sandeshak4777@gmail.com>
2022-10-04 06:01:39 +0530

    reversed linked list

--- Week-7/exercise7.1/reverselinkedlist.js
+++ Week-7/exercise7.1/reverselinkedlist.js
@@ -19,10 +19,26 @@ d.next = e;
 const printLinkedListElement = (head) => {
   let current = head;
   while (current !== null) {
-    console.log("values", current.value);
+    // console.log("values", current.value);
     current = current.next;
   }
 };
 
 printLinkedListElement(a);
 
+const reverseLinkedListElements = (head) => {
+  let prev = null;
+  let current = head;
+  while (current !== null) {
+    const next = current.next;
+    console.log("values", current.value);
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+  head = prev;
+  return head;
+};
+
+const reverse = reverseLinkedListElements(a);
+console.log("reverse", reverse);
