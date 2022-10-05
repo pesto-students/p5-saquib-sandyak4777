7eed7d76b006be839c3d234ca6e8dcdbccfe5682
sandyak4777 <sandeshak4777@gmail.com>
2022-10-05 15:04:52 +0530

    added detect loop logic

--- Week-7/exercise7.3/Detectloops.js
+++ Week-7/exercise7.3/Detectloops.js
@@ -15,7 +15,25 @@ const push = (data) =>{
     head = new_node;
 }
 
+function detectLoop() {
+    let tortoisePointer = head,
+      rabitPointer = head;
+  
+    while (tortoisePointer != null && rabitPointer != null && rabitPointer.next != null) {
+      tortoisePointer = tortoisePointer.next;
+      rabitPointer = rabitPointer.next.next;
+      if (tortoisePointer == rabitPointer) {
+        return true;
+      }
+    }
+  }
+
 push(20);
 push(4);
 push(15);
 push(10);
+
+head.next.next.next.next = head;
+
+const result = detectLoop();
+console.log("result", result);
