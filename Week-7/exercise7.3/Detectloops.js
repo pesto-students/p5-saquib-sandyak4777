8c98975f37f42549b07cc0393de268f7a9b4b53b
sandyak4777 <sandeshak4777@gmail.com>
2022-10-05 15:02:06 +0530

    implemented push function

--- Week-7/exercise7.3/Detectloops.js
+++ Week-7/exercise7.3/Detectloops.js
@@ -8,3 +8,14 @@ class Node{
 }
 
 head=null;
+
+const push = (data) =>{
+    let new_node = new Node(data);
+    new_node.next = head;
+    head = new_node;
+}
+
+push(20);
+push(4);
+push(15);
+push(10);
